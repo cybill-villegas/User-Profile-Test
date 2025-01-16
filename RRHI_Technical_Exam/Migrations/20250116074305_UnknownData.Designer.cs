8be3ac6a69d30dc9c4f68f466bbbd9fe94daf4f9
cybill-villegas <61441155+cybill-villegas@users.noreply.github.com>
2025-01-16 15:43:39 +0800

    Unknown

--- RRHI_Technical_Exam/Migrations/20250116074305_UnknownData.Designer.cs
+++ RRHI_Technical_Exam/Migrations/20250116074305_UnknownData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RRHI_Technical_Exam.Data;
 
@@ -10,9 +11,11 @@ using RRHI_Technical_Exam.Data;
 namespace RRHI_Technical_Exam.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250116074305_UnknownData")]
+    partial class UnknownData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
