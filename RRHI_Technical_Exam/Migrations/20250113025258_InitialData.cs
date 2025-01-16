5fe50ced2995fcf27cb90c3541e209055c8422f2
cybill-villegas <61441155+cybill-villegas@users.noreply.github.com>
2025-01-16 15:41:06 +0800

    initialdata

---
+++ RRHI_Technical_Exam/Migrations/20250113025258_InitialData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RRHI_Technical_Exam.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValueSql: "GETDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValueSql: "GETDATE()");
+        }
+    }
+}
